0ab3ac9361f6a8468a884694038e84fb8ec22008
Bruce <bruce.hillsworth@gmail.com>
2023-05-03 13:43:55 -0600

    Passed all tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+import ipdb
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word)
+
+    def match(self, word_list):
+        return list(word for word in word_list if sorted(word) == self.word)
